0da88a1354eb03f71bc9b46ac64c16d074047485
fpl_proleague <strederk6@gmail.com>
2026-07-26 11:27:07 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import FDRTool from './FDRTool.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <FDRTool />
+    <Analytics />
   </React.StrictMode>,
 )
