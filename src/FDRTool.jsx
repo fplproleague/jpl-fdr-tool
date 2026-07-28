cf7da635672c6aa4c703a7fc850872e9e34cb0e8
fpl_proleague <strederk6@gmail.com>
2026-07-28 19:46:25 +0200

    Update FDRTool.jsx

--- src/FDRTool.jsx
+++ src/FDRTool.jsx
@@ -1198,7 +1198,7 @@ export default function FDRTool() {
                   <input
                     type="text" required value={newPlayerName}
                     onChange={e => setNewPlayerName(e.target.value)}
-                    placeholder="Bv Zorgane"
+                    placeholder="Bv. Zorgane"
                     style={watchlistInputStyle}
                   />
                 </label>
