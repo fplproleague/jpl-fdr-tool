50dff867379e23c0bbfd08097cdbc94af3b1ca5d
fpl_proleague <strederk6@gmail.com>
2026-07-29 03:20:47 +0200

    Update FDRTool.jsx

--- src/FDRTool.jsx
+++ src/FDRTool.jsx
@@ -856,7 +856,7 @@ export default function FDRTool() {
           <img
             src="/app-icon-mark.png"
             alt=""
-            style={{ width: '44px', height: '44px', borderRadius: '2px', flexShrink: 0, marginTop: '-2px'}}
+            style={{ width: '44px', height: '44px', borderRadius: '2px', flexShrink: 0, marginTop: '30px'}}
             onError={(e) => { e.target.style.display = 'none'; }}
           />
           <div>
